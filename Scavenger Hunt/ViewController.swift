7399acf845fdc7863359ed8259ccc6ec00f63f7b
Apple <apple@apples-macbook-10.local>
2016-02-04 18:45:15 -0500

    Initial Commit

---
+++ Scavenger Hunt/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Scavenger Hunt
+//
+//  Created by Apple on 2/4/16.
+//  Copyright © 2016 Chandler Pergakis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
